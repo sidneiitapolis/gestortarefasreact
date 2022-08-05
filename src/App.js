2307e6d821c6fe6a0589b2c8ab39cd80e9e64f5e
Your Name <sidnei_itapolis@hotmail.com>
2022-08-05 11:24:55 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import  {useState,useRef} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//state
+const [listaTarefa,setListaTarefas]=useState(()=>{return []})
+const [tarefa,setTarefa]=useState(()=>{return''})
+//ref
+const idTarefa=useRef(0)
+
+//funçao adicionar tarefa
+
+function adicionarTarefa(){
+  setListaTarefas(old =>{ return [...old, {id:idTarefa.current,texto:tarefa}]})
+  idTarefa.current = idTarefa.current + 1
 }
+
+function limparTarefas(){
+  setListaTarefas(old =>{return[]})
+  idTarefa.current = 0
+}
+
+  function removerTarefa(iten){    
+    
+    const tmp = listaTarefa.filter((tarefa)=>{
+      return tarefa.id !== iten
+    })
+    
+    setListaTarefas(tmp)
+    
+  }
+
+
+  return (
+    <div>
+        <h3>Gestor de Tarefas</h3>
+        <br/>
+        <hr/>
+        <div>
+          <input type='text' value={tarefa} onChange={(evento)=>{setTarefa(evento.target.value)}}/>
+            <button onClick={adicionarTarefa}>Adicinar</button>
+            <button onClick={limparTarefas}>Limpar </button>
+        </div>
+        <br/>
+        <hr/>
+        <p>Tarefas</p>       
+        {listaTarefa.map((tarefa)=>{
+          return  <p key={tarefa.id}>{tarefa.texto} - <span onClick={()=>{removerTarefa(tarefa.id)}}>|Remover|</span></p>         
+        })}
+    </div>
+    
+  )}
 
 export default App;
