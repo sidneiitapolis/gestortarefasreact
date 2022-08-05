9d8287a1f498b7c264a888e42d48e9450aeb5640
Your Name <sidnei_itapolis@hotmail.com>
2022-08-05 11:50:43 -0300

    Updates

---
+++ precache-manifest.1363633357fbf2988310bc0135110fa0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c9d12f8d3edd22bc4b8",
+    "url": "/gestortarefasreact/static/js/main.3c9d12f8.chunk.js"
+  },
+  {
+    "revision": "12a547d5be2359b88d04",
+    "url": "/gestortarefasreact/static/js/1.12a547d5.chunk.js"
+  },
+  {
+    "revision": "63670c303a076fbdb110",
+    "url": "/gestortarefasreact/static/js/runtime~main.63670c30.js"
+  },
+  {
+    "revision": "58505471be931c8262f1e664df9fde70",
+    "url": "/gestortarefasreact/index.html"
+  }
+];
